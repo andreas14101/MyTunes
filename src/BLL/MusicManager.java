7502545069e67933efa8b2b212db9b28115879ba
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2022-11-29 12:37:22 +0100

    still working on it

--- src/BLL/MusicManager.java
+++ src/BLL/MusicManager.java
@@ -1,5 +1,6 @@
 package BLL;
 
+import BE.Song;
 import DAL.ICRUDPlaylist;
 import DAL.ICRUDSongs;
 import DAL.MusicDAO;
@@ -13,4 +14,9 @@ public class MusicManager {
         songDAO = new MusicDAO();
         playlistDAO = new MusicDAO();
     }
+
+    public Song createSong(String title, String artist, int length, String category, String pathToFile) throws Exception {
+        return songDAO.createSong(title, artist, length, category, pathToFile);
+    }
+
 }
--- src/GUI/Controller/SongViewController.java
+++ src/GUI/Controller/SongViewController.java
@@ -1,8 +1,35 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+
 public class SongViewController extends BaseController {
+
+    @FXML
+    private TextField songTitleTxt;
+    @FXML
+    private TextField artistTxt;
+    @FXML
+    private TextField fileTxt;
+
     @Override
     public void setup() {
+
+    }
+
+    @FXML
+    private void handleSave(ActionEvent actionEvent) {
+
+        String title = songTitleTxt.getText();
+        String artist = artistTxt.getText();
+        String pathToFile = fileTxt.getText();
+
+        File file = new File(pathToFile);
+
+
 
     }
 }
