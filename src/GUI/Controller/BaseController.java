f6e0ac9367aa3f3a78ec96918bc070268520af59
andreas14101 <andreasshansen@live.dk>
2022-11-29 09:49:44 +0100

    update

--- src/GUI/Controller/BaseController.java
+++ src/GUI/Controller/BaseController.java
@@ -5,7 +5,7 @@ import GUI.Model.MyTunesModel;
 public abstract class BaseController {
 private MyTunesModel model;
 
-private void setModel(MyTunesModel model) {this.model = model;}
+public void setModel(MyTunesModel model) {this.model = model;}
 public  MyTunesModel getModel() {return model;}
 public  abstract void setup();
 }
--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,11 +1,32 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import javax.swing.text.TabExpander;
+import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController extends BaseController implements Initializable {
+
+    public TextField filterSearch;
+    public Slider timeSlider;
+    public TableView songsTable;
+    public TableView playlistTable;
+    public Slider volumeSlider;
+
     @Override
     public void setup() {
 
@@ -16,4 +37,41 @@ public class MainViewController extends BaseController implements Initializable 
 
     }
 
+    public void handleNewSong(ActionEvent event) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/GUI/View/SongView.fxml"));
+        AnchorPane pane = (AnchorPane) loader.load();
+
+        SongViewController controller = loader.getController();
+        controller.setup();
+
+        // Create the dialog stage
+        Stage dialogWindow = new Stage();
+        dialogWindow.setTitle("New song");
+        dialogWindow.initModality(Modality.WINDOW_MODAL);
+        dialogWindow.initOwner(((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(pane);
+        dialogWindow.setScene(scene);
+        dialogWindow.showAndWait();
+    }
+
+
+    public void handleNewPlaylist(ActionEvent event)  throws  IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/GUI/View/PlaylistView.fxml"));
+        AnchorPane pane = (AnchorPane) loader.load();
+
+        PlaylistViewController controller = loader.getController();
+        controller.setup();
+
+        // Create the dialog stage
+        Stage dialogWindow = new Stage();
+        dialogWindow.setTitle("New playlist");
+        dialogWindow.initModality(Modality.WINDOW_MODAL);
+        dialogWindow.initOwner(((Node)event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(pane);
+        dialogWindow.setScene(scene);
+        dialogWindow.showAndWait();
+    }
 }
