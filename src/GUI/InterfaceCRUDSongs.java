6651cc4b243f6893cf07b9c3a2aa5a57db484ea0
TheDemon12345678910 <anehoimark@gmail.com>
2022-11-29 08:48:59 +0100

    InterfaceCreated

---
+++ src/GUI/InterfaceCRUDSongs.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import java.util.List;
+
+public interface InterfaceCRUDSongs {
+    public List<Song> getAllSongs() throws Exception;
+
+    public Song createSong(String title, String artist, );
+
+
+
+
+}
