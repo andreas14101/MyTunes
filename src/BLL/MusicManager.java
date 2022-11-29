93753e123b8a85fc495a5ec5efa4a9c79ac62ba4
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2022-11-29 09:33:57 +0100

    Connection to DAL
    setup connection to dal with a variable and creating a new instance of it, in the contructor

--- src/BLL/MusicManager.java
+++ src/BLL/MusicManager.java
@@ -1,4 +1,12 @@
 package BLL;
 
+import DAL.MusicDAO;
+
 public class MusicManager {
+
+    private MusicDAO musicDAO;
+
+    public MusicManager() {
+        musicDAO = new MusicDAO();
+    }
 }
--- src/DAL/MusicDAO.java
+++ src/DAL/MusicDAO.java
@@ -6,4 +6,6 @@ public class MusicDAO {
     public MusicDAO() {
         databaseConnector = new MyDatabaseConnector();
     }
+
+
 }
